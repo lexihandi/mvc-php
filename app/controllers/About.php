2ca8242e54990b2df6b14f81c078ee923dd95222
lexyhandi <lexihandi@gmail.com>
2021-02-20 17:21:33 +0700

    update

---
+++ app/controllers/About.php
@@ -0,0 +1,15 @@
+<?php
+
+class About
+{
+
+    public function index($nama = "coba", $pekerjaan = "coba")
+    {
+        echo "Halo nama saya $nama dan pekerjaan saya adalah $pekerjaan";
+    }
+
+    public function page()
+    {
+        echo 'about/Page';
+    }
+}
